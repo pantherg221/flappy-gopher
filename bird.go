68c472c3cfef18911d12e9b7a47dc47458dbf6a3
Jonathan Goodwin <njo.goodwin@gmail.com>
2017-11-13 17:00:04 -0800

    handle multiple events and bird bounce

--- bird.go
+++ bird.go
@@ -7,9 +7,16 @@ import (
 	"github.com/veandco/go-sdl2/sdl"
 )
 
+const (
+	gravity   = 0.25
+	jumpSpeed = 5
+)
+
 type bird struct {
 	time     int
 	textures []*sdl.Texture
+	y        float64
+	speed    float64
 }
 
 func newBird(r *sdl.Renderer) (*bird, error) {
@@ -22,12 +29,19 @@ func newBird(r *sdl.Renderer) (*bird, error) {
 		}
 		textures = append(textures, texture)
 	}
-	return &bird{textures: textures}, nil
+	return &bird{textures: textures, y: 300}, nil
 }
 
 func (b *bird) paint(r *sdl.Renderer) error {
 	b.time++
-	rect := &sdl.Rect{X: 10, Y: 300 - 43/2, W: 50, H: 43}
+	b.y -= b.speed
+	if b.y < 0 {
+		b.speed = -b.speed
+		b.y = 0
+	}
+	b.speed += gravity
+
+	rect := &sdl.Rect{X: 10, Y: (600 - int32(b.y)) - 43/2, W: 50, H: 43}
 
 	i := b.time / 10 % len(b.textures)
 	if err := r.Copy(b.textures[i], nil, rect); err != nil {
@@ -40,4 +54,8 @@ func (b *bird) destroy() {
 	for _, t := range b.textures {
 		t.Destroy()
 	}
+}
+
+func (b *bird) jump() {
+	b.speed = -jumpSpeed
 }
